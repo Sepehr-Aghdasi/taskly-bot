e38900ba6d1005f86149a66c87f692f5dd0c028e
Sepehr-Aghdasi <Sepehr.Aghdasi82@gmail.com>
2026-01-04 11:30:32 +0330

    Import PrismaService into app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 
 import { AppService } from './app.service';
 import { UserService } from './user/user.service';
+import { PrismaService } from './prisma/prisma.service';
 import { TelegramService } from './telegram/telegram.service';
 import { TaskSchedulerService } from './task-scheduler/task-scheduler.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, UserService, TelegramService, TaskSchedulerService],
+  providers: [AppService, PrismaService, UserService, TelegramService, TaskSchedulerService],
 })
 export class AppModule { }
